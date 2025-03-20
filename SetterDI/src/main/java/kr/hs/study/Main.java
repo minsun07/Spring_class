f44c49cf906d96c41bf597e86706666fecf81098
Minsun An <elleto123456@gmail.com>
2025-03-20 14:09:54 +0900

    setterDI 객체 참조

--- SetterDI/src/main/java/kr/hs/study/Main.java
+++ SetterDI/src/main/java/kr/hs/study/Main.java
@@ -1,6 +1,7 @@
 package kr.hs.study;
 
 import kr.hs.study.beans.TestBean;
+import kr.hs.study.beans.TestBean2;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -15,6 +16,9 @@ public class Main {
         System.out.println("data3 : " +  obj1.isData3());
         System.out.println("data4 : " + obj1.getData4());
 
+        TestBean2 obj2 = ctx.getBean("t2", TestBean2.class);
+        System.out.println("data1 : " + obj2.getData1());
+        System.out.println("data2 : " + obj2.getData2());
 
 
         ctx.close();
